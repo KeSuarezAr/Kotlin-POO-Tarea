6a978a3e79fc9082fde505e25061b9aedeef27d9
KeSuarezAr <kesuarezar@uide.edu.ec>
2023-12-06 18:55:19 -0500

    creada la clase de Persona y se demostro ejemplos

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,16 @@
 fun main() {
-    val name = "Kotlin"
 
-    println("Hello, $name")
+    val persona1 = Persona("Kevin", 19, "Estudiante")
+    val persona2 = Persona("Juan", 20, "Estudiante")
+    val persona3 = Persona("Pedro", 21, "Estudiante")
 
-    for (i in 1..5) {
+    val estudiantes = listOf(persona1, persona2, persona3)
 
-        println("i = $i")
+    for (estudiante in estudiantes) {
+        println(
+                "Mi nombre es ${estudiante.nombre}, tengo ${estudiante.edad} años y mi ocupacion es ${estudiante.ocupacion}"
+        )
     }
 }
+
+class Persona(val nombre: String, val edad: Int, val ocupacion: String)
