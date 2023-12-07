570c472e45f5ee7fc3473337918c5bbed20dd7d7
KeSuarezAr <kesuarezar@uide.edu.ec>
2023-12-06 19:26:14 -0500

    implementacion de la interfaz de vehiculo y sus clases coche y bicicleta

--- src/Main.kt
+++ src/Main.kt
@@ -11,6 +11,29 @@ fun main() {
                 "Mi nombre es ${estudiante.nombre}, tengo ${estudiante.edad} años y mi ocupacion es ${estudiante.ocupacion}"
         )
     }
+
+    val coche = Coche()
+    val bicicleta = Bicicleta()
+
+    coche.acelerar()
+    bicicleta.acelerar()
 }
 
 class Persona(val nombre: String, val edad: Int, val ocupacion: String)
+
+interface Vehiculo {
+    fun acelerar()
+}
+
+class Coche : Vehiculo {
+    override fun acelerar() {
+        println("El coche está acelerando.")
+    }
+}
+
+class Bicicleta : Vehiculo {
+    override fun acelerar() {
+        println("La vicicleta esta siendo pedaleada.")
+    }
+}
+
